af85baab5d8e2873d1cc8146c3c1a303e6d46faa
Copeland Carter <smartycope@gmail.com>
2023-11-09 16:19:57 -0700

    Small updates, added Dockerfile section

--- dockerPresentation.py
+++ dockerPresentation.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
+st.caption('Team Marquet')
 st.image('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-world.net%2Fwp-content%2Fuploads%2F2021%2F02%2FDocker-Symbol.png&f=1&nofb=1&ipt=5a14afd1e119e1ac15314fcdfdbb1773fa696a5236965cb353f443f7e97f5c97&ipo=images', width=400)
+
 '# Docker'
 st.caption('What is it? What does it do? How does it work?')
 
@@ -8,8 +10,9 @@ st.caption('What is it? What does it do? How does it work?')
 """
 ### Docker is *not* a virtual machine.
 It's also not quite emulation software. It's kind of it's own niche which it fills nicely.
+Which is why it's used by over 20 million developers in more than 7 million applications.
 
-It's kind of like a virtual machine for a specific process.
+You can kind of think of it as a lightweight virtual machine for a specific process.
 """
 
 with st.expander('Purpose'):
@@ -35,4 +38,20 @@ with st.expander('Containers and Images'):
 with st.expander('Integration'):
     """
     It has a fully featured VSCode extension and integrates seemlessly with GitHub
+    """
+
+with st.expander('Dockerfile'):
+    """
+    The heart of using Docker is the file called `Dockerfile`. In a Dockerfile, you specify the image,
+    which, when instantiated, becomes a runnable container.
+    Here, we'll show you the basic commands used in Dockerfiles.
+    """
+
+    st.caption('These don\'t *have* to be uppercase, but they always are by convention')
+
+    # Docs are here: https://docs.docker.com/engine/reference/builder/#from
+    """
+    - `FROM`
+        - Always the very first line in a file\*, it specifies the base image from the docker website
+        - `<example>`
     """
